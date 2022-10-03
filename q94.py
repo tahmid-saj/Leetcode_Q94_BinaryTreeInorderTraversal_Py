011f55b30c7b524d2c878f1db53eb18f535a5a15
Tahmid Sajin <64264899+tahmid-saj@users.noreply.github.com>
2022-10-03 02:25:44 -0400

    Create q94.py

---
+++ q94.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def helper(self, root, result):
+        if root == None:
+            return
+        
+        self.helper(root.left, result)
+        result.append(root.val)
+        self.helper(root.right, result)
+        
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        self.helper(root, result)
+        return result
